4aa38a8707a3ba2ac31f620ea13766cdc8cfbeab
Batman <bgmyhan@gmail.com>
2020-07-06 12:53:28 -0700

    created bear bones skeletons files

---
+++ config/orm.js
@@ -0,0 +1,37 @@
+const connection = require ("connection");
+
+sqlFuntions = {
+     selectAll: function (){
+
+     },
+     insertOne: function (){
+          
+     },
+     updateOne: function (){
+          
+     }
+}
+ 
+
+      module.exports(sqlFunctions)
+//      * 
+//      *    * Export the ORM object in `module.exports`.
+//      * 
+//      * const {selectAll, instertOne, updateOne,} = 
+
+
+
+
+// 3. Create an `orm.js` file inside `config` directory.
+
+//    * Import (require) `connection.js` into `orm.js`
+
+//    * In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+
+//      * `selectAll()`
+//      * `insertOne()`
+//      * `updateOne()`
+
+//    * Export the ORM object in `module.exports`.
+
+// #### Model setup
---
+++ db/seeds.sql
@@ -0,0 +1,33 @@
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Bubba", false)
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Double Double", true)
+ 
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Double Double", true)
+
+
+ write insert queries to populate the `burgers` table with at least three entries.
+
+ -- INSERT INTO products (flavor, price, quantity)
+-- VALUES ("vanilla", 2.50, 100);
+
+-- INSERT INTO products (flavor, price, quantity)
+-- VALUES ("chocolate", 3.10, 120);
+
+-- INSERT INTO products (flavor, price, quantity)
+-- VALUES ("strawberry", 3.25, 75);
+
+-- -- ### Alternative way to insert more than one row
+-- -- INSERT INTO products (flavor, price, quantity)
+-- -- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
+
+   
+   
+--    * Create the `burgers_db`.
+--    * Switch to or use the `burgers_db`.
+--    * Create a `burgers` table with these fields:
+--      * **id**: an auto incrementing int that serves as the primary key.
+--      * **burger_name**: a string.
+--      * **devoured**: a boolean. -->

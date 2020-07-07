6f666385517b3a6fd48779b0860e1a3398261fa3
Batman <bgmyhan@gmail.com>
2020-07-07 08:52:46 -0700

    Updated methods to update SQL tables. Still need to add handlebars

--- config/orm.js
+++ config/orm.js
@@ -1,37 +1,49 @@
-const connection = require ("connection");
+const connection = require ("./connection");
+const { connect } = require("./connection");
 
-sqlFuntions = {
-     selectAll: function (){
+// sqlFuntions = {
+//      selectAll: function (){
 
-     },
-     insertOne: function (){
+//      },
+//      insertOne: function (){
           
-     },
-     updateOne: function (){
+//      },
+//      updateOne: function (){
           
-     }
+//      }
+// }
+function selectAll(){
+connection.query('SELECT * FROM burgers', (err,rows) => {
+     if(err) throw err;
+   
+     console.log('Data received from Db:');
+     console.log(rows);
+   });
 }
- 
+     //  module.exports(connection.query)
+function insertOne(){
 
-      module.exports(sqlFunctions)
-//      * 
-//      *    * Export the ORM object in `module.exports`.
-//      * 
-//      * const {selectAll, instertOne, updateOne,} = 
+     let insert = `INSERT INTO burgers(burger_name, devoured)
+     VALUES({{variable}}, false)`;
+
+connection.query(insert)
+connect.end
+
+}
+function updateOne(){
+     let update = `UPDATE burgers SET completed = ? WHERE id = ?`;
+     connection.query(update)
+     connect.end
+}
 
 
+// connection.query('INSERT INTO burgers WHERE burger_name', (err,rows) => {
+
+//      INSERT INTO authors (id, name, city)
 
 
-// 3. Create an `orm.js` file inside `config` directory.
-
-//    * Import (require) `connection.js` into `orm.js`
-
-//    * In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
-
-//      * `selectAll()`
-//      * `insertOne()`
-//      * `updateOne()`
-
-//    * Export the ORM object in `module.exports`.
-
-// #### Model setup
+//      if(err) throw err;
+   
+//      console.log('Data received from Db:');
+//      console.log(rows);
+//    });
